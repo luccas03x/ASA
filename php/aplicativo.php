9c22c216b6b237791c6aea33a95f9e4c4af51968
Luccas Alberto <136741892+luccas03x@users.noreply.github.com>
2023-11-14 19:29:39 -0300

    Add files via upload

---
+++ php/aplicativo.php
@@ -0,0 +1,109 @@
+<meta charset="UTF-g">
+
+<html>
+
+    <title> BlueAccess </title>
+
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/Footer-with-button-logo.css">
+    </head>
+
+    <body>
+
+        <div class="container">
+            <center>
+                <a href="index.php"> <img src="img/BANNER.png" width="1350" height="250"> </a>
+            </center>
+            <hr>
+            <div class="row" style="background-color: rgba(50,50,205,0.7);height:40px;padding-top:5px;">
+
+                <div class="col-md-3 text-center"> <a href="produto.php"><font color=black><b>Produto</b></font> </a></div>
+                <div class="col-md-3 text-center"><a href="aplicativo.php"> <font color=black><b>Aplicativo</b></font></a></div>
+                <div class="col-md-3 text-center"><a href="manual.php"> <font color=black><b>Manual de Instruções</b></font></a></div>
+                <div class="col-md-3 text-center"><a href="midias.php"> <font color=black><b>Mídias Sociais</b></font></a></div>
+
+            </div>
+            <hr>  
+
+
+            <div class="container">
+                <h2>BlueAccess</h2>    
+                <br>
+                <div class="col-md-1">
+                    <img src="img/aplicativo.png" width="333" style="float:left"></a>
+                </div>
+                <div class="col-md-11">
+                    <br><br><br><br>
+                    <div class="card">
+                        <div class="card-body">
+                            <p style="content: justify;"> <b>BlueAccess</b> é o nome do nosso aplicativo conta com apenas uma tela e opera de uma forma bem simples mas muito objetiva, que contém:  </p>
+                            <br>
+                            <p>    • O botão da <b>Câmera</b> para acessar a câmera da frente da sua casa;  </p>
+                            <p>    • O status do portão, avisando se ele está aberto ou fechado;  </p>
+                            <p>    • Os botões <b>Conectar Bluetooth</b> e <b>Desconectar Bluetooth</b>;  </p>
+                            <p>    • Os dois botões mais importantes que é <b>Abrir Portão</b> e <b>Fechar Portão</b>. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br> <br> <br><br>
+            <hr>
+        </div>
+
+
+        <footer id="myFooter">
+            <div class="row" style="background-color: rgba(50,50,205,0.3)">
+                <div class="col-sm-3">
+                    <center>
+                        <img src="img/logo pronto png.png" width="180">
+                    </center>
+                </div>
+                <div class="col-sm-3">
+                    <br>
+                    <h5><b>Início</b></h5>
+                    <ul>
+                        <li><a href="index.php"><font color=black>Home</font></a></li>
+                        <li><a href="produto.php"><font color=black>Produto</font></a></li>
+                        <li><a href="aplicativo.php"><font color=black>Aplicativo</font></a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <br>
+                    <h5><b>Sobre Nós</b></h5>
+                    <ul>
+                        <li><a href="index.php"><font color=black>Informações</font></a></li>
+                        <li><a href="midias.php"><font color=black>Mídias Sociais</font></a></li>
+                        <li><a href="manual.php"><font color=black>Manual de Instruções</font></a></li>
+                    </ul>
+                </div>
+                <br>
+                <div class="col-sm-3">
+                    <div class="social-networks">
+                        <br>
+                        <span style="font-size: 65px"><a href="https://www.facebook.com/ASA-Automatic-Security-Access-112255113478062/" class="facebook"><i class="fa fa-facebook" class="fa fa-list-alt fa-2x"></i></a></span>
+                        <span style="font-size: 65px"><a href="https://www.instagram.com/empresa_asa/?hl=pt" class="instagram"><i class="fa fa-instagram" class="fa fa-list-alt fa-2x"></i></a></span>
+                    </div>
+                    <a href="midias.php"><button type="button" class="btn btn-primary" style="width:105">Contato</button></a>
+                </div>
+            </div>
+        </div>
+        <center>
+            <div class="footer-copyright" style="background-color: rgba(50,50,205,0.7)">
+                <br>
+                <h5>© 2019 Copyright | ASA - Automatic Security Access</h5>
+                <br>
+            </div>
+        </center>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+
+
+</body>
+</html>
---
+++ php/produto.php
@@ -0,0 +1,133 @@
+<meta charset="UTF-g">
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/Footer-with-button-logo.css">
+    </head>
+
+    <body>
+
+        <div class="container">
+            <center>
+                <a href="index.php"> <img src="img/BANNER.png" width="1350" height="250"> </a>
+            </center>
+            <hr>
+            <div style="background-color: rgba(50,50,205,0.7);height:40px;padding-top:5px;" class="row">
+
+                <div class="col-md-3 text-center"> <a href="produto.php"><font color=black><b>Produto</b></font> </a></div>
+                <div class="col-md-3 text-center"><a href="aplicativo.php"> <font color=black><b>Aplicativo</b></font></a></div>
+                <div class="col-md-3 text-center"><a href="manual.php"> <font color=black><b>Manual de Instruções</b></font></a></div>
+                <div class="col-md-3 text-center"><a href="midias.php"> <font color=black><b>Mídias Sociais</b></font></a></div>
+
+            </div>
+
+            <hr>
+
+            <div class="container">
+                <font align="justify">
+                <h2> BlueAccess </h2>
+                <br>
+                <div class="col-md-1">
+                    <img src="img/key.png" width="250" style="float:left"></a>
+                </div>
+                <br>
+                <div class="col-md-11">
+                    <p style="content: justify;">O nosso produto é o aplicativo <a href=aplicativo.php><b><font color=black>BlueAccess</font></b></a>, que abre o portão automático da sua casa ou condomínio via bluetooth. Pode ser aberto também via cartão magnético.</p>
+                    <p style="content: justify;">Entretanto, nosso produto já existe, porém nosso serviço possui alguns diferenciais em relação aos outros, como por exemplo uma câmera instalada pra ter uma visão da frente da sua casa e também o cartão magnético.</p>
+                </div>
+                <br><br><br>
+
+
+                <hr>
+                <h3> Cartão Magnético </h3>
+                <br>
+                <div class="col-md-1">
+                    <img src="img/key card (3).png" width="250"style="float:left"></a>
+                </div>
+                <br><br>
+                <div class="col-md-11">
+
+                    <p style="content: justify;">Como dito acima, nosso portão automático abre via cartão magnético também.</p>
+                    <p style="content: justify;">O cartão magnético é pequeno tanto que da para colocar no seu chaveiro, ele seria usado para pedestres e para casos de "emergência" caso o seu dispositivo móvel acabe a bateria.</p>
+                </div>
+
+                <br><br>
+
+
+
+                <hr>
+                <h3> Câmera </h3>
+                <br>
+                <div class="col-md-1">
+                    <img src="img/camera de security.png" width="250" style="float:left"></a>
+                </div>
+                <div class="col-md-11">
+                    <br>
+                    <p style="content: justify;">Como dito anteriormente, nosso portão automático terá uma câmera HiCam P2P instalada para ter uma visão da frente da sua casa.</p>
+                    <p style="content: justify;">A câmera trará uma funcionalidade a mais para o nosso produto, trazendo segurança, onde o cliente poderá acessar imagens da câmera de qualquer lugar através do nosso aplicativo, BlueAccess; além de fornecer funções extras como captura de áudio e visão noturna.</p>
+                </div>
+                <br>
+                <hr>
+            </div>
+        </div>
+        </font>
+
+
+
+
+
+        <footer id="myFooter">
+            <div class="row" style="background-color: rgba(50,50,205,0.3)">
+                <div class="col-sm-3">
+                    <center>
+                        <img src="img/logo pronto png.png" width="180">
+                    </center>
+                </div>
+                <div class="col-sm-3">
+                    <br>
+                    <h5><b>Início</b></h5>
+                    <ul>
+                        <li><a href="index.php"><font color=black>Home</font></a></li>
+                        <li><a href="produto.php"><font color=black>Produto</font></a></li>
+                        <li><a href="aplicativo.php"><font color=black>Aplicativo</font></a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <br>
+                    <h5><b>Sobre Nós</b></h5>
+                    <ul>
+                        <li><a href="index.php"><font color=black>Informações</font></a></li>
+                        <li><a href="midias.php"><font color=black>Mídias Sociais</font></a></li>
+                        <li><a href="manual.php"><font color=black>Manual de Instruções</font></a></li>
+                    </ul>
+                </div>
+                <br>
+                <div class="col-sm-3">
+                    <div class="social-networks">
+                        <br>
+                        <span style="font-size: 65px"><a href="https://www.facebook.com/ASA-Automatic-Security-Access-112255113478062/" class="facebook"><i class="fa fa-facebook" class="fa fa-list-alt fa-2x"></i></a></span>
+                        <span style="font-size: 65px"><a href="https://www.instagram.com/empresa_asa/?hl=pt" class="instagram"><i class="fa fa-instagram" class="fa fa-list-alt fa-2x"></i></a></span>
+                    </div>
+                    <a href="midias.php"><button type="button" class="btn btn-primary" style="width:105">Contato</button></a>
+                </div>
+            </div>
+        </div>
+        <center>
+            <div class="footer-copyright" style="background-color: rgba(50,50,205,0.7)">
+                <br>
+                <h5>© 2019 Copyright | ASA - Automatic Security Access</h5>
+                <br>
+            </div>
+        </center>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+
+
+</body>
+</html>
